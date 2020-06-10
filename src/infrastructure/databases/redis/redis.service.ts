920ae7fb0cbb347c9a330da3d6fa45ef7881490d
Dmitry Belousov <dmitry1998.ua@outlook.com>
2020-06-10 13:30:57 +0300

    added redis database

---
+++ src/infrastructure/databases/redis/redis.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import * as redis from 'redis';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class RedisService {
+  private client: redis.RedisClient;
+  constructor(private readonly configService: ConfigService) {
+    this.client = redis.createClient({
+      host: configService.get<string>('REDIS_HOST'),
+      port: configService.get<number>('REDIS_PORT'),
+      db: configService.get<number>('REDIS_DB'),
+    });
+  }
+
+  async set(key: string, value: any, duration: number): Promise<boolean> {
+    return this.client.set(key, JSON.stringify(value), 'EX', duration, e => {
+      if (e) {
+        throw new Error(e.toString());
+      }
+    });
+  }
+
+  async exist(key: string): Promise<boolean> {
+    const promise = new Promise<boolean>((resolve, reject) => {
+      this.client.exists(key, (e, ok) => {
+        resolve(Boolean(ok));
+        reject(e);
+      });
+    });
+
+    return promise;
+  }
+
+  async get(key: string): Promise<any> {
+    return this.client.get(key, e => {
+      if (e) {
+        throw new Error(e.toString());
+      }
+    });
+  }
+
+  async delete(key: string): Promise<boolean> {
+    return this.client.del(key, e => {
+      if (e) {
+        throw new Error(e.toString());
+      }
+    });
+  }
+}
