1ec6d49d40b1c49a759a6f30c05a06dfda400ac0
Dmitry Belousov <dmitry1998.ua@outlook.com>
2020-07-21 15:46:55 +0300

    added validation modelId before getting document by ID

--- src/infrastructure/databases/mongoose/repository/base.repository.ts
+++ src/infrastructure/databases/mongoose/repository/base.repository.ts
@@ -1,8 +1,10 @@
-import { PaginateModel, Document, FilterQuery } from 'mongoose';
+import { PaginateModel, Document, FilterQuery, Types } from 'mongoose';
 
 import { PaginationOptions } from '../pagination/paginate.params';
 import { IBaseRepository } from '../../base.repository.interface';
 import { IPaginate } from '../pagination/pagination.output';
+import { BadRequestException } from '@nestjs/common';
+import { dictionary } from '../../../../config/dictionary';
 
 export class BaseRepository<T extends Document> implements IBaseRepository<T> {
   protected model: PaginateModel<T>;
@@ -12,7 +14,11 @@ export class BaseRepository<T extends Document> implements IBaseRepository<T> {
   }
 
   async findById(id: string): Promise<T> {
-    return await this.model.findById(id).exec();
+    if (Types.ObjectId.isValid(id)) {
+      return await this.model.findById(id).exec();
+    }
+
+    throw new BadRequestException(dictionary.errors.incorrectId);
   }
 
   async delete(_id: string): Promise<any> {
